bd852f01a08c44378b859906145f50b1d92368ea
Dhruv Rabadiya <dhrvrabadiya@gmail.com>
2024-05-15 13:14:20 +0530

    controller and route created and add migration for login

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,13 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\loginController;
+use App\Http\Controllers\ProfileController;
 Route::get('/', function () {
-    return view('welcome');
+    return view('login');
 });
+
+
+Route::get('/profile', [loginController::class, 'login']);
+Route::get('/profile', [ProfileController::class, 'profile']);
+
